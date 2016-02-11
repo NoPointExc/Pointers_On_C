b6f8d1233dc7fe9572b7915c69b73cd74fa74b09
Sun Jiayang <a13813258259@163.com>
2016-02-10 22:01:47 -0500

    finish chp5 exc5 without test

--- chp5/exc5.c
+++ chp5/exc5.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 
 unsigned  getMask(unsigned  start,unsigned  end);
+int storeBitField(int originalVal, int valToStore, unsigned startBit, unsigned endBit);
 
 void main(void){
 	unsigned start,end;
@@ -10,6 +11,23 @@ void main(void){
 	ans=getMask(start,end);
 	printf("%d\n",ans);
 }
+
+int storeBitField(int originalVal, int valToStore, unsigned startBit, unsigned endBit){
+	//1.get mask
+	unsigned mask=getMask(startBit,endBit);
+	//2.originalVal AND mask
+	int rst;
+	rst=originalVal&mask;	 
+	int i;
+	//3. move
+	for(i=0;i<endBit;i++) valToStore<<=1;
+	//4.
+	valToStore&=mask;
+	//5. OR
+	rst|=valToStore;	
+	return rst;
+}
+
 
 unsigned getMask(unsigned start,unsigned end){
 	int i;
