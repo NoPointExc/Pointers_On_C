d9e18ed08eab1633ac35aed3c59f770d49f566d1
Sun Jiayang <a13813258259@163.com>
2016-02-25 15:05:44 +0800

    fix last value to NULL

--- chp11/exc4.c
+++ chp11/exc4.c
@@ -8,32 +8,42 @@ Node* create_list(int *vals, int size);
 
 
 void main(void){
- 	int vals[]={1,2,3,4,5};
+ 	//int vals[]={1,2,3,4,5};
+ 	
+ 	int *vals=NULL;
  	int size=sizeof(vals)/sizeof(int);
  	Node* head=create_list(vals,size);
  	if(head==NULL) printf("%s\n","head==NULL");
  	Node *p=head;
 	int i;
 
-	for(i=0;i<5;i++){
-		printf("%i\n",p->val);
+	while(p!=NULL){
+		printf("%i\n",p->val );
 		p=p->next;
 	}
- 	
+
+		
+
+
 }
 
 Node* create_list(int *vals, int size){
 	Node *list=(Node*)malloc(size*sizeof(Node));
-	if(list==NULL) return NULL;
-	Node *head=list;
-	int i;
-	
+	if(list==NULL || vals==NULL) return NULL;
+	Node *head=list;	
+	int i;	
 	for(i=0;i<size;i++){
-		if(i<size) list->next=&head[i+1];		
+		/**
+		 * point to next node
+		 */
+		if(i<size-1) list->next=&head[i+1];		
 		else list->next=NULL;
+		/**
+		 * assign val
+		 */
 		list->val=vals[i];
 		list=list->next;
 	}
-
+	
 	return head;
 }
