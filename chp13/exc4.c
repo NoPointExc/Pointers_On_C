abe05302beff621b9ef1f095a6d3420acb6e6ef9
Sun <jiayang.sun@intel.com>
2016-03-12 19:00:21 +0800

    pointer of pointer not fully understand yet

--- chp13/exc4.c
+++ chp13/exc4.c
@@ -1,25 +1,33 @@
 #include <stdio.h>
 
 int compare(void const *p0, void const *p1);
-void sort(void *head, int len, int size, int (*comp)(void const*p0, void const *p1));
-void print(char const *head,int len);
+void sort(void *head[], int len, int size, int (*comp)(void const*p0, void const *p1) );
+void _sort(void *head[], int left, int right, int (*comp)(void const*p0, void const *p1) );
+void swap(void *p0, void *p1);
+void print(void const *head[],int len);
 
+/*
+* the value of argc(memory-address) is not continue, so never use argv or pass *argv instand of argv
+**/
 int main(int argc, char const *argv[])
 {	
 	int i;
-	for(i=0;i<argc;i++) printf("%c\n",*argv[i]);
-	print(*argv[0],argc);
+	print( (void *)argv,argc);
+	swap((void *)argv[2],(void *)argv[3]);
+	print( (void *)argv,argc);
 	return 0;
 }
 
 //quick sort
-void sort(void *head, int len, int size, int (*comp)(void const*p0, void const *p1))
+void sort(void *head[], int len, int size, int (*comp)(void const*p0, void const *p1))
 {
-	if(len==0|| len==1) return;
-	int mid=len/2;
-	sort(head,mid,size,comp);
-	sort(head+mid*size,len-mid,size,comp);
+
 }
+
+void _sort(void *head[], int left, int right, int (*comp)(void const*p0, void const *p1) ){
+	if(left >=right) return;
+}
+
 
 int compare(void const *p0, void const *p1)
 {
@@ -29,8 +37,14 @@ int compare(void const *p0, void const *p1)
 	return val0>val1;
 }
 
-void print(char const *head, int len){
+void swap(void *p0, void *p1)
+{
+	//printf("%d\n",(int)*p0);
+}
+
+void print(void const *head[], int const len)
+{
 	int i;
-	for(i=0;i<len;i++) printf("%c, ",head[i]);
+	for(i=1;i<len;i++) printf("%d , ", ((char *)head)[i]);
 	printf("\n");
 }
