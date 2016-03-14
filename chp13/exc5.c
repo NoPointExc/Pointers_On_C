27b94a3042a990208eb7aa8a0e00fc1c645fc6b9
jiayang <a13813258259@163.com>
2016-03-14 18:17:56 +0800

    - can be value

--- chp13/exc5.c
+++ chp13/exc5.c
@@ -1,3 +1,6 @@
+/**
+*TO FIX: argument stop before -
+*/
 #include <stdio.h>
 
 #define NOT_CTR 0
@@ -5,11 +8,12 @@
 #define PLUS_CTR 2
 
 #define NUL '\0'
-
+#define MAX_VAL_LEN 100
 char ** do_args( int argc, char  **argv, char *control, 
 void (*do_arg)( int ch, char * value ),
 void (*illegal_arg)( int ch ) );
 int whats_ctr(char c,char *control);
+char * get_value(char *this_str, char *next_str);
 //test
 void do_arg(int ch,char *value);
 void illegal_arg(int ch);
@@ -17,7 +21,7 @@ void illegal_arg(int ch);
 int main(int argc, char  *argv[])
 {
 	
-	char *control="x+y+z+";
+	char *control="x+y+z+abc";
 	int i;
 	for(i=1;i<argc;i++){
 		do_args(argc,argv,control,do_arg,illegal_arg);
@@ -26,52 +30,12 @@ int main(int argc, char  *argv[])
 }
 
 //argv[istr][ichar]
-
 char ** do_args( int argc, char  **argv, char *control, 
 void (*do_arg)( int ch, char * value ),
 void (*illegal_arg)( int ch ) )
 {
-	int istr=1;
-
-	while(istr<argc){
-		char *str=argv[istr];
-
-		while(*str!=NUL){
-			if(str[0]=='-'){
-				int ctr_type=whats_ctr(*(str+1), control);
-				if(ctr_type==NOT_CTR){
-					illegal_arg( *(str+1) );
-					return &argv[istr+1]; //return next str
-				}else if(ctr_type==EMPTY_CTR){
-					do_arg( *(str+1),NULL);
-					str+=2;
-				}else if(ctr_type==PLUS_CTR){
-					if(*(str+2)==NUL){
-						if(istr>=argc){
-							illegal_arg( *(str+1) );
-							return &argv[istr+1]; //return next str
-						}else{
-							//have a argument
-							istr+=1;
-							do_arg( *(str+1), argv[istr]);
-							istr+=1;
-							break; //go to next str
-						}
-					}else{
-						do_arg( *(str+1), (str+2));
-						str+=3;
-						break;
-					}
-				}
-			}else{
-				return &argv[istr+1];
-			}
-		}
-
-		istr+=1;
-	}
-		
-	return &argv[istr+1]; //return next str
+	
+	return argv;
 }
 
 /**
@@ -91,7 +55,6 @@ int whats_ctr(char c,char *control)
 	}
 	return NOT_CTR;
 }
-
 
 //test
 
